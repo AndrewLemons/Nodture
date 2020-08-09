74c9f384589cd6769bd583f566e28127a71b43dd
Andrew <andrewglemons@gmail.com>
2020-08-09 11:26:56 -0400

    Build foundation
    A simple foundation with minimal functionality.

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {};
+config.mysql = {};
+
+config.port = '80';
+config.storageDir = '/users/andrew/Library/Application Support/Nodture';
+
+config.mysql.host = '192.168.68.130';
+config.mysql.user = 'nodtureuser';
+config.mysql.pwd = '1234';
+config.mysql.db = 'nodture';
+
+module.exports = config;
